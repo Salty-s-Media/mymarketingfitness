b7ea3b2fac1f5bcb1d0480ec0d993b55c53b8984
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-04-30 15:24:43 -0700

    static SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,43 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.mymarketingfitness.com"),
   title: "My Marketing Fitness",
   description: "My Marketing Fitness: Marketing for Roofers and Contractors",
+  keywords: [
+    "roofing marketing",
+    "contractor marketing",
+    "roofing SEO",
+    "contractor SEO",
+    "roofing digital marketing",
+    "contractor digital marketing",
+    "roofing online marketing",
+    "contractor online marketing",
+    "roofing marketing strategies",
+    "contractor marketing strategies",
+    "roofing marketing tips",
+    "contractor marketing tips",
+    "roofing marketing plan",
+    "contractor marketing plan",
+    "roofing marketing tools",
+    "contractor marketing tools",
+    "roofing marketing software",
+  ],
+  openGraph: {
+    title: "My Marketing Fitness",
+    description:
+      "Discover the best marketing strategies for roofers and contractors. Boost your online presence with our comprehensive marketing solutions.",
+    images: [
+      {
+        url: "/logo.svg",
+        width: 217,
+        height: 58,
+        alt: "My Marketing Fitness Logo",
+      },
+    ],
+    url: "https://www.mymarketingfitness.com",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
